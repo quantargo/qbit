95a91475527de236913ec1e0bb41eb30a89106ec
mannau <mario.annau@quantargo.com>
2021-07-03 17:29:54 +0200

    Include qbit::render

--- R/invoke.R
+++ R/invoke.R
@@ -1,43 +1,48 @@
-# Invoke QBit
-#
-# QBit workspaces can be completely controlled via API including not only code
-# executions but also tasks like file upload/download, workspace export, etc.
-# Note, that this function is intended for internal use only.
-#
-# @param qbit_id character; Name of the QBit function.
-# @param event_type character; Event type to be used. Can be one of the following:
-# - qbit-run: Run selected code in qbit.
-# - qbit-fork: Create a new QBit from template
-# - qbit-remove: Remove QBit
-# - qbit-render: Render markdown document.
-# - qbit-packages-install: Install packages in qbit
-# - qbit-packages-remove: Remove packages from QBit
-# - qbit-fetch-result: Fetch result from QBit
-# - state-reset: Remove console outputs from QBit
-# - qbit-update-state
-# - qbit-download-workspace
-# - upload-request
-# - upload-finished
-# - file-delete
-# @param event_input list;
-# @param stateful logical;
-# @param apikey character; API Key used to invoke QBit API endpoint.
-# After creating an account at \url{https://www.quantargo.com} the API key is available
-# in the user settings \url{https://www.quantargo.com/dashboard}.
-# @examples
-# \dontrun{
-#   options(QKEY = "<YOUR-API-KEY>")
-#   qbit:::invoke("qbit-template-r-base", "qbit-run", list(code = "1+1"))
-#   qbitnew <- qbit:::invoke("qbit-template-r-base", "qbit-fork", list(qbitId = "qbit-template-r-base", qbitName = "TEST QBit"))
-#   qbit:::invoke(qbitnew$newQbitId, "qbit-run", list(code = "1+1")) # TODO: Check if there is console output
-#   qbit:::invoke(qbitnew$newQbitId, "qbit-remove", list(qbitId = qbitnew$newQbitId))
-#   qbitnew$newQbitId
-# }
+#' Invoke QBit
+#'
+#' QBit workspaces can be completely controlled via API including not only code
+#' executions but also tasks like file upload/download, workspace export, etc.
+#' Note, that this function is intended for internal use only.
+#'
+#' @param qbit_id character; Name of the QBit function.
+#' @param event_type character; Event type to be used. Can be one of the following:
+#' - qbit-run: Run selected code in qbit.
+#' - qbit-fork: Create a new QBit from template
+#' - qbit-remove: Remove QBit
+#' - qbit-render: Render markdown document.
+#' - qbit-packages-install: Install packages in qbit
+#' - qbit-packages-remove: Remove packages from QBit
+#' - qbit-fetch-result: Fetch result from QBit
+#' - state-reset: Remove console outputs from QBit
+#' - qbit-update-state
+#' - qbit-download-workspace
+#' - upload-request
+#' - upload-finished
+#' - file-delete
+#' @param event_input list;
+#' @param stateful logical;
+#' @param apikey character; API Key used to invoke QBit API endpoint.
+#' After creating an account at \url{https://www.quantargo.com} the API key
+#' is available
+#' in the user settings \url{https://www.quantargo.com/dashboard}.
+#' @param verbose logical; Show intermediary console outputs.
+#' @examples
+#' \dontrun{
+#'   options(QKEY = "<YOUR-API-KEY>")
+#'   qbit:::invoke("qbit-template-r-base", "qbit-run", list(code = "1+1"))
+#'   qbitnew <- qbit:::invoke("qbit-template-r-base", "qbit-fork",
+#'   list(qbitId = "qbit-template-r-base", qbitName = "TEST QBit"))
+# TODO: Check if there is console output
+#'   qbit:::invoke(qbitnew$newQbitId, "qbit-run", list(code = "1+1"))
+#'   qbit:::invoke(qbitnew$newQbitId, "qbit-remove", list(qbitId = qbitnew$newQbitId))
+#'   qbitnew$newQbitId
+#' }
 #' @importFrom httr POST PUT add_headers content
 #' @importFrom jsonlite unbox
+#' @export
 invoke <- function(qbit_id,
                    event_type,
-                   event_input,
+                   event_input = NULL,
                    stateful = FALSE,
                    apikey = getOption("QKEY"),
                    verbose = getOption("verbose")) {
@@ -66,10 +71,10 @@ invoke <- function(qbit_id,
   h <- list(`x-api-key` = apikey)
 
   body_invoke <- list(eventType = event_type,
-                      eventInput = event_input,
                       stateful = stateful)
 
   body_invoke$qbitId <- qbit_id
+  body_invoke$eventInput <- event_input
 
   body_invoke_json <- jsonlite::toJSON(body_invoke, auto_unbox = TRUE)
 
@@ -78,7 +83,13 @@ invoke <- function(qbit_id,
   if (!is.null(resp_invoke_content$error)) {
     stop(sprintf("ERROR: %s", resp_invoke_content$error))
   }
-  jsonlite::fromJSON(resp_invoke_content$body)
+
+  if (!is.null(resp_invoke_content$body)) {
+    resp_invoke_content <- jsonlite::fromJSON(resp_invoke_content$body)
+  } else {
+    stop(sprintf("ERROR: %s", resp_invoke_content$message))
+  }
+  resp_invoke_content
 }
 
 #' Create a New QBit based on template
@@ -87,10 +98,11 @@ invoke <- function(qbit_id,
 #' @param template character; Id of template to be copied/forked from. Can be any id accessible
 #' at quantargo, see also \url{https://www.quantargo.com/qbits/explore}
 #' @param verbose logical; Show intermediary console outputs.
+#' @param ... Additional parameters passed to \link{invoke}
 #' @return Id of created workspace
 #' @export
-create <- function(qbit_name, template = get_qbit_template("r"), verbose = getOption("verbose")) {
-  qbitnew <- invoke(template, "qbit-fork", list(qbitId = template, qbitName = qbit_name))
+create <- function(qbit_name, template = get_qbit_template("r"), verbose = getOption("verbose"), ...) {
+  qbitnew <- invoke(template, "qbit-fork", list(qbitId = template, qbitName = qbit_name), verbose = verbose, ...)
   qbit_id <- qbitnew$newQbitId
   if(verbose) message(sprintf("New QBit '%s' successfully created.\nVisit https://www.quantargo.com/qbits/%s to view your workspace.", qbit_id, qbit_id))
   qbit_id
@@ -100,10 +112,11 @@ create <- function(qbit_name, template = get_qbit_template("r"), verbose = getOp
 #'
 #' @param qbit_id character; Workspace Id to be removed.
 #' @param verbose logical; Show intermediary console outputs.
-#' @return New QBit id
+#' @param ... Additional parameters passed to \link{invoke}
+#' @return TRUE (invisible) in case of success
 #' @export
-remove <- function(qbit_id, verbose = getOption("verbose")) {
-  out <- invoke(qbit_id, "qbit-fork", list(qbitId = qbit_id))
+remove <- function(qbit_id, verbose = getOption("verbose"), ...) {
+  out <- invoke(qbit_id, "qbit-fork", list(qbitId = qbit_id), verbose = verbose, ...)
   if(verbose) message(sprintf("QBit '%s' successfully removed.", qbit_id))
   invisible(TRUE)
 }
@@ -130,5 +143,15 @@ get_qbit_template <- function(short_name) {
   named_templates[match(short_name, names(named_templates))]
 }
 
-
-
+#' Render RMarkdown Document
+#'
+#' @param qbit_id character; Workspace Id to be removed.
+#' @param verbose logical; Show intermediary console outputs.
+#' @param ... Additional parameters passed to \link{invoke}
+#' @return list including response object
+#' @export
+render <- function(qbit_id, verbose = getOption("verbose"), ...) {
+  out <- invoke(qbit_id, "qbit-render", verbose = verbose, ...)
+  if(verbose) message(sprintf("QBit '%s' successfully removed.", qbit_id))
+  out
+}
--- R/zzz.R
+++ R/zzz.R
@@ -2,11 +2,7 @@
   if (Sys.getenv("QKEY") != "") {
     options(QKEY = Sys.getenv("QKEY"))
   }
-
-  options(STAGE = Sys.getenv("STAGE", "dev"))
-  if (options("STAGE") == "dev") {
-    options(QBITURL = "https://api.quantargo.com/lambda-dev")
-  } else if (options("STAGE") == "prod") {
-    options(QBITURL = "https://api.quantargo.com/lambda")
+  if (Sys.getenv("QBITURL") != "") {
+    options(QBITURL = Sys.getenv("QBITURL"))
   }
 }
