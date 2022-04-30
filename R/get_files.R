0fada581a55c2632dabc4a68c3f6a92fd0fd61e8
mannau <mario.annau@quantargo.com>
2022-04-30 21:35:55 +0200

    Update get_files, include main folder

--- R/get_files.R
+++ R/get_files.R
@@ -7,9 +7,6 @@ get_asset_files <- function(path = ".") {
   out <- list.files(path, pattern = "[^(html)|(Rmd)|(json)|(.DS_Store)]$", recursive = TRUE, full.names = TRUE)
   out <- grep("_cache", out, fixed = TRUE, value = TRUE, invert = TRUE)
   out <- grep("./renv", out, fixed = TRUE, value = TRUE, invert = TRUE)
-  out <- grep("\\.\\/\\d+", out, value = TRUE)
-  if (file.exists(file.path(path, "main"))) {
-    out <- c(out, "main")
-  }
+  out <- grep("./qbit.*", out, value = TRUE, invert = TRUE)
   out
 }
